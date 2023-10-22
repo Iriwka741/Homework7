430867592c2b350f00a87471e1d87ed90a61a7f9
Lenovo <katashirina89@gmail.com>
2023-10-22 18:57:53 +0300

    Implemented first task

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 #print("Hello teacher")
-print("Homework №7 Banul")
+#print("Homework №7 Banul")
 
+#1.Написати рекурсивну функцію знаходження ступеня числа.
+
+def get_num_pow(number, degree):
+     if degree <= 1:
+         return number
+
+     return number * get_num_pow(number, degree - 1)
+
+print(get_num_pow(3, 4))
+# get_num_pow(3, 4) -> 3 * get_num_pow(3, 3) => 81
+# get_num_pow(3, 3) -> 3 * get_num_pow(3, 2) => 27
+# get_num_pow(3, 2) -> 3 * get_num_pow(3, 1) => 9
+# get_num_pow(3, 1) => 3
